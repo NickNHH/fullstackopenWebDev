260f39a90b72317ba743c4d87b15c0b00f0814a8
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-04-24 16:43:17 +0200

    Use states

--- src/main/java/part1/intro/src/index.js
+++ src/main/java/part1/intro/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 const Hello = (props) => {
-    const {name, age} = props; 
+    const {name, age} = props;
     const bornYear = () => new Date().getFullYear() - age;
 
     return (
@@ -16,12 +16,22 @@ const Hello = (props) => {
 const App = () => {
     const name = "Peter";
     const age = "10";
+    const [counter, setCounter] = useState(0);
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    );
+
+    console.log('rendering...', counter);
 
     return (
         <>
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10}/>
             <Hello name={name} age={age}/>
+            <br/>
+            <div>{counter}</div>
         </>
     )
 };
